aaf1ab669f28e4c310da7d19c1896e757252e573
Nikolas Savvidis <nicos.savidis@gmail.com>
2018-08-21 20:30:41 +0200

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import buble from 'rollup-plugin-buble';
 import fileSize from 'rollup-plugin-filesize';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
 export default [
